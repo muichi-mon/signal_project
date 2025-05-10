e7396ebe4a698490cca9d24bc3024171f0788fcb
Issven <rajveerjadhav08@gmail.com>
2025-05-10 12:24:19 +0200

    updating changes to start working on week 3 tasks.

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -23,7 +23,7 @@ class DataStorageTest {
 
         List<PatientRecord> records = storage.getRecords(1, 1714376789050L, 1714376789051L);
         System.out.println(records.size());
-        // assertEquals(2, records.size()); // Check if two records are retrieved
-        // assertEquals(100.0, records.get(0).getMeasurementValue()); // Validate first record
+        //assertEquals(2, records.size()); // Check if two records are retrieved
+        //assertEquals(100.0, records.get(0).getMeasurementValue()); // Validate first record
     }
 }
